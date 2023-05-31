baab1cffb0c7e59f4967ae3815eed304730238ca
VladSolyankin <darnell.dewberry@mail.ru>
2023-05-31 13:16:33 +0300

    Added new styles in companies.css, new comms in companiesHandler.py

--- bottle_design_company/companiesHandler.py
+++ bottle_design_company/companiesHandler.py
@@ -5,18 +5,23 @@ from bottle import post, request, datetime, template
 
 @post('/companies', method='post')
 def companiesHandler():
+    # Receiving form data
     companyTitle = request.forms.get("TITLE")
     companyDescription = request.forms.get("DESCRIPTION")
     companyPhone = request.forms.get("PHONE")
     companyImage = request.files.get("IMAGE")
     
+    # Saving loaded images in folder
     start_path = f"./static/companyImages/{companyImage.filename}"
     if not os.path.exists(start_path):
         companyImage.save(start_path)
     else:
+        # Returns template with error
         return template('companies.tpl', error='Company already exists!')
 
+    # Writing data about new company in text file
     with open('companies.txt', 'a') as f:
         f.write(f"\n./static/companyImages/{companyImage.filename}:{companyTitle}:{companyDescription}:{companyPhone}")
 
+    # Returns updated template
     return template('companies.tpl', error='')
