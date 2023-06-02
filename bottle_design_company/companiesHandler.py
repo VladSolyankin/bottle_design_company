a4bc56e7da2b6939f4974e30066bf3254cfdce5e
VladSolyankin <darnell.dewberry@mail.ru>
2023-06-02 20:16:37 +0300

    Added comments for companiesHandler.py

--- bottle_design_company/companiesHandler.py
+++ bottle_design_company/companiesHandler.py
@@ -1,8 +1,11 @@
 from operator import contains
 import os
 from bottle import post, request, datetime, template
+import re
+import string
 
 
+# Function for new company form validation
 @post('/companies', method='post')
 def companiesHandler():
     # Receiving form data
@@ -10,6 +13,10 @@ def companiesHandler():
     companyDescription = request.forms.get("DESCRIPTION")
     companyPhone = request.forms.get("PHONE")
     companyImage = request.files.get("IMAGE")
+
+    # Checking for filling all input fields
+    if not len(companyTitle) or not len(companyDescription) or not len(companyPhone):
+        return template('companies.tpl', error='Fill all the fields!')
     
     # Saving loaded images in folder
     start_path = f"./static/companyImages/{companyImage.filename}"
@@ -28,20 +35,41 @@ def companiesHandler():
 
 
 
-def testFormTitle():
-    # Receiving form data
-    companyTitle = request.forms.get("TITLE")
+# Function for new company title test
+def testFormTitle(newCompanyTitleList):
+    result = []
+    regex = re.compile(r'[A-Za-z]+')
+    for i in newCompanyTitleList:
+        # False if title too short, number-only or doesn't match regex-pattern
+        if len(i) < 2 or i.isnumeric() or not re.fullmatch(regex, i):
+            result.append(False)
+        else:
+            result.append(True)
+    return result        
 
-    if len(companyTitle) < 2 or not companyTitle[0].isupper():
-         return False
 
-    return True
-   
+# Function for new company description test
+def testFormDescription(newCompanyDescriptionList):
+    result = []
+    regex = re.compile(r'([A-Za-z]+\s)([0-9!@#$%^&*()]*)')
+    for i in newCompanyDescriptionList:
+        # False if title too short or number-only
+        if len(i) < 10 or i.isnumeric() or not re.match(regex, i):
+            result.append(False)
+        else:
+            result.append(True)
+    return result
 
-def testFormDescription():
-    companyDescription = request.forms.get("DESCRIPTION")
+# Function for new company phone
+def testFormPhone(newCompanyPhoneList):
+    regex = re.compile(r'8-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}')
+    result = []
 
-    if len(companyDescription) < 10:
-        return False
+    for i in newCompanyPhoneList:
+        # False if phone isn't matching regex-pattern
+        if not re.fullmatch(regex, i):
+            result.append(False)
+        else:
+            result.append(True)
 
-    return True
+    return result
